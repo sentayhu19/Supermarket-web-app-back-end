66105a1e4570cdf9f082f647bfdd3dff404fe982
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-10-07 17:11:04 +0300

    product methods updated

--- app/controllers/V1/api/products_controller.rb
+++ app/controllers/V1/api/products_controller.rb
@@ -1,9 +1,28 @@
 class V1::Api::ProductsController < ApplicationController
     def index
-
+        @products = Product.all
+        if @products
+            render json: @products
+        else
+            render json: {error: "No products found"}
+        end
     end
-    def new 
+    def new
+        @product = Product.new
     end
     def create
+        product = Product.new(product_params)
+        if product.save
+            render json: product
+        else
+            render json: {error: "Product not saved"}
+        end
+    def show
+        @product = Product.find(params[:id])
+        if @product
+            render json: @product
+        else
+            render json: {error: "Product not found"}
+        end
     end
 end
